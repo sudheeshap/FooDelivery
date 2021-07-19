b9242fa677c3bfa7a162c75e2e84aebed26c600b
Sudheesh Parambil <hellosudhee@gmail.com>
2021-07-19 21:20:56 +0400

    Added style guide

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,9 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello</p>
         <a
           className="App-link"
           href="https://reactjs.org"
