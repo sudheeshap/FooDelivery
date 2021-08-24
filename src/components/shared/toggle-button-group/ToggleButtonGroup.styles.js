899c1bd6d664e4198fe6711b7fe6734179bbc7e3
Sudheesh Parambil <hellosudhee@gmail.com>
2021-08-24 12:25:04 +0400

    Added ToggleButtonGroup component

---
+++ src/components/shared/toggle-button-group/ToggleButtonGroup.styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+import breakpoints from '../../../styles/breakpoints';
+import Button from '../button/Button';
+
+export const ToggleButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  align-items: flex-start;
+  flex-direction: column;
+
+  @media screen and ${breakpoints.device.md} {
+    align-items: center;
+    flex-direction: row;
+  }
+`;
+
+export const ToggleButton = styled(Button)`
+  margin-bottom: 8px;
+
+  @media screen and ${breakpoints.device.md} {
+    margin-right: 8px;
+  }
+`;
